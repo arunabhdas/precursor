47b954b98d4ba0f07f4faf352e42eb40b5c589b8
Arunabh Das <arunabhdas@gmail.com>
2019-11-05 00:11:53 -0500

    Reset and create from NativeScript Sidekick

---
+++ ProblemSolver/src/care/care-card/care-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "CareCard",
+    templateUrl: "./care-card.component.html",
+    styleUrls: ["../care-common.css"]
+})
+export class CareCardComponent { }
